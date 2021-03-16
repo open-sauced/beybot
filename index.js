b19f2ac5eff3ec5581f83add6420eef726c2f6ab
Brian Douglas <bdougie@users.noreply.github.com>
2021-03-16 11:40:10 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 /* Config */
-const twitchTvHandle = "bdougieYO";
+const twitchTvHandle = "githubeducation";
 const PAUSE_DURATION = 30 * 1000; // 30 seconds
 const DISPLAY_DURATION = 10 * 1000; // 10 seconds
 
